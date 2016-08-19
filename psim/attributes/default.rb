87237ee265e087834ce6ca08a11519eb33356479
stairsj <jstairs@printeron.com>
2016-08-19 14:10:39 -0400

    Adding default attributes for pas_config.

--- psim/attributes/default.rb
+++ psim/attributes/default.rb
@@ -1,5 +1,6 @@
 default['psim']['installer_url'] = "https://s3.amazonaws.com/chef-psim/PSIM.exe"
 default['psim']['install_dir'] = "C:\\Program Files (x86)\\PrinterOn Corporation"
+default['psim']['data_dir'] = "C:\\ProgramData\\PrinterOn Corporation\\PONData"
 
 default['psim']['host']['serialnbr']['pas'] = "HMNJ-Y054-XWD6"
 default['psim']['host']['serialnbr']['pds'] = "1YMW-0HDJ-3M2H"
@@ -11,3 +12,6 @@ default['psim']['cps']['http_port'] = 80
 default['psim']['cps']['https_port'] = 443
 default['psim']['cps']['sslProtocols'] = "SSLv3,TLSv1,TLSv1.1,TLSv1.2"
 default['psim']['cps']['keystoreSettings'] = 'keystoreFile="${pon.data.root}\KeyStore\keystore"  keystorePass="rz6KZSpMD7fy7Co6UfIBmw%3D%3D"'
+
+default['psim']['pas']['processing']['layer'] = 'pas'
+default['psim']['pas']['status']['layer'] = 'pas'
